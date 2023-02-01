a2dcb2e9c80945276b08bf276a38ec65ec4a4c93
Fatih <fatih_orucu@hotmail.com>
2023-02-01 15:14:57 +0300

    Mongoose DB added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -9,13 +10,28 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 let postArray = []
 const app = express();
 
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+mongoose.set("strictQuery",false);
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const postSchema = new mongoose.Schema({
+  title: String,
+  post: String,
+  kebabTitle: String
+})
+const Post = mongoose.model("Post", postSchema);
+
 app.get("/",function(req,res){
-  res.render("home",{content: homeStartingContent, postArray: postArray});
+  Post.find(function(err,result){
+    if (!err) {
+      res.render("home",{content: homeStartingContent, postArray: result});
+    }
+  })
+  
 })
 app.get("/about",function(req,res){
   res.render("about",{content: aboutContent});
@@ -27,35 +43,28 @@ app.get("/compose",function(req,res){
   res.render("compose");
 })
 app.get("/posts/:postid",function(req,res){
-  postArray.forEach(element => {
     let header = req.params.postid;
-    let postTitle = element.title;
-    let post = element.post;
-    if (element.kebabTitle === header) {
-      res.render("post",{postTitle: postTitle, post:post})
-    }
-  });
+    Post.findOne({kebabTitle: header},function(err,result){
+      if (!err) {
+        let postTitle = result.title;
+        let post = result.post;
+        res.render("post",{postTitle: postTitle, post:post})
+      }
+    })
 })
 
 app.post("/compose", function(req,res){
-  var post = {
+  var post = new Post({
     title: req.body.titleText,
     post: req.body.postText,
     kebabTitle: _.kebabCase(req.body.titleText)
-  }
-  postArray.push(post)
-  res.redirect("/")
+  })
+  post.save(function(err,result){
+    if (!err) {
+      res.redirect("/")
+    }
+  })
 })
-
-
-
-
-
-
-
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
